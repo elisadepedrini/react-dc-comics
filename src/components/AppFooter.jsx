47ba05fca966bbbe539deb403eaf0d665c7ded29
elisadepedrini <elisa.depedrini@gmail.com>
2026-04-15 12:33:37 +0200

    Create AppHeader component

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,5 @@
+export default function AppFooter() {
+    return(
+        <h1>footer</h1>
+    )
+}
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,5 @@
+export default function AppMain() {
+    return (
+        <h1>main</h1>
+    )
+}
