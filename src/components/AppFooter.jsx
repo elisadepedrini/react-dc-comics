717b3d9817c47344b82c0c7a13d37bc103c1ab38
elisadepedrini <elisa.depedrini@gmail.com>
2026-04-15 16:09:43 +0200

    Create AppFooter component

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,55 @@
 export default function AppFooter() {
     return(
-        <h1>footer</h1>
+        <>
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Comics</a></li>
+                            <li><a href="">Movies</a></li>
+                            <li><a href="">TV</a></li>
+                            <li><a href="">Games</a></li>
+                            <li><a href="">Videos</a></li>
+                            <li><a href="">News</a></li>
+                        </ul>
+                        <h4>SHOP </h4>
+                        <ul>
+                            <li><a href="">Shop DC</a></li>
+                            <li><a href="">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h4>DC</h4>
+                        <ul>
+                            <li><a href="">Terms Of Use</a></li>
+                            <li><a href="">Privacy Policy</a></li>
+                            <li><a href="">Ad Choices</a></li>
+                            <li><a href="">Advertising</a></li>
+                            <li><a href="">Jobs</a></li>
+                            <li><a href="">Subscriptions</a></li>
+                            <li><a href="">Talent Workshop</a></li>
+                            <li><a href="">CPSC Certificates</a></li>
+                            <li><a href="">Ratings</a></li>
+                            <li><a href="">Shop Help</a></li>
+                            <li><a href="">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h4>SITES</h4>
+                        <ul>
+                            <li><a href="">DC</a></li>
+                            <li><a href="">MAD Magazine</a></li>
+                            <li><a href="">DC Kids</a></li>
+                            <li><a href="">DC Universe</a></li>
+                            <li><a href="">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        </>
     )
 }
